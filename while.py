35f760dc09f031285a5b836af978acc24d547297
Mika Stenberg <mika.stenberg@laurea.fi>
2023-04-13 13:45:42 +0300

    add

--- while.py
+++ while.py
@@ -1,4 +1,12 @@
-pin = int(input("Anna pin-koodi: "))
 
+
+# Eka versio
 while pin != 1234:
     pin = int(input("Anna pin-koodi: "))
+
+# Toinen versio
+while True:
+    pin = int(input("Anna pin-koodi: "))
+    if pin == 3210:
+        print("Nyt meni oikein!")
+        break
