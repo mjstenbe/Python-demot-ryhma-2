af6468e7dada202973d721b93256fa05b528b013
Mika Stenberg <mika.stenberg@laurea.fi>
2024-05-06 11:14:19 +0000

    add

--- sananelio.py
+++ sananelio.py
@@ -1,9 +1,13 @@
 def nelio(sana, koko):
+    # Indeksien alku ja loppu
     a = 0
     l = koko
+    # Tulostetaan rivejä koko kertaa
     for i in range(koko):
         rivi = sana*10
+        # Tulostetaan rivi indeksien a ja l väliltä
         print(rivi[a:l])
+        # Kasvatetaan indeksien paikkaa sanan pituuden verran
         a+=koko
         l+=koko
 
